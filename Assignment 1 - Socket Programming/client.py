b9269d20fefee8606c9236a95569fc4333ed2c60
soedomoto <soedomoto@gmail.com>
2016-02-27 10:50:11 +0700

    Add thread support

--- Assignment 1 - Socket Programming/client.py
+++ Assignment 1 - Socket Programming/client.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
 
-import socket
+import socket, sys
 
-# Define addr where socket server is listening
-host = socket.gethostname()
-port = 12345
+# Use argument to define port
+args = sys.argv
+if not len(args) == 3:
+    print 'Usage : python client.py <host> <port>'
+    exit()
+else:
+    # Define addr where socket server is listening
+    host = args[1]
+    port = int(args[2])
 
 # Create socket client
-s = socket.socket()
-s.connect((host, port))
+sock = socket.socket()
+sock.connect((host, port))
+
+# Say hello to server
+sock.send("Hai server...")
 
 # Print incoming messages
-print s.recv(1024)
-s.close
+msg = sock.recv(1024)
+print "[+] %s:%s - Received message : %s" % (host, port, msg)
+
+sock.close
--- Assignment 1 - Socket Programming/server.py
+++ Assignment 1 - Socket Programming/server.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python
 
-import socket
+import socket, threading, sys
 
-# Define addr where socket server is listening
-host = socket.gethostname()
-port = 12345
+# Use argument to define port
+args = sys.argv
+if not len(args) == 2:
+    print 'Usage : python server.py <port>'
+    exit()
+else:
+    # Define addr where socket server is listening
+    host = '0.0.0.0'
+    port = int(args[1])
+
+# Create thread class to handle each connected client
+class ClientThread(threading.Thread):
+    def __init__(self, ip, port, socket):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.socket = socket
+        print "[+] %s:%s - New thread is started" % (ip, str(port))
+
+    def run(self):
+        # Define send and recv handler for each thread
+        self.socket.send('Welcome, Thank you for connecting...')
+        msg = self.socket.recv(1024)
+        print "[+] %s:%s - Received message : %s" % (ip, str(port), msg)
 
 # Create socket server
-s = socket.socket()
-s.bind((host, port))
-s.listen(5)
+sock_server = socket.socket()
+sock_server.bind((host, port))
+sock_server.listen(5)
+print 'Socket server is listening at %s:%s' % (host, port)
 
 # Wait for client
 while True:
-    c, addr = s.accept()
-    print 'Got connection from', addr
-    c.send('Thank you for connecting')
-    c.close()
+    (sock, (ip, port)) = sock_server.accept()
+    thread = ClientThread(ip, port, sock)
+    thread.start()
