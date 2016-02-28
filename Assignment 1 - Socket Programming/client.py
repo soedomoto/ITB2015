05d7f7db1d5e3ecf789370a082ce6fa9b999f474
soedomoto <soedomoto@gmail.com>
2016-02-28 13:51:48 +0700

    Using Non-blocking Socket

--- Assignment 1 - Socket Programming/client.py
+++ Assignment 1 - Socket Programming/client.py
@@ -1,26 +1,55 @@
 #!/usr/bin/python
 
-import socket, sys
+import socket, threading, sys, select
 
-# Use argument to define port
-args = sys.argv
-if not len(args) == 3:
-    print 'Usage : python client.py <host> <port>'
-    exit()
-else:
-    # Define addr where socket server is listening
-    host = args[1]
-    port = int(args[2])
+recv_buffer = 4096
 
-# Create socket client
-sock = socket.socket()
-sock.connect((host, port))
+def prompt():
+    sys.stdout.write('[Me]\t\t\t: '); 
+    sys.stdout.flush()
 
-# Say hello to server
-sock.send("Hai server...")
+if __name__ == "__main__":
+    # Use argument to define port
+    args = sys.argv
+    if not len(args) == 3:
+        print 'Usage : python client.py <host> <port>'
+        exit()
+    else:
+        # Define addr where socket server is listening
+        host = args[1]
+        port = int(args[2])
 
-# Print incoming messages
-msg = sock.recv(1024)
-print "[+] %s:%s - Received message : %s" % (host, port, msg)
-
-sock.close
+    # Create socket client
+    sock = socket.socket()
+    try:
+        sock.connect((host, port))
+    except socket.error:
+        print 'Unable to connect'
+        exit()
+    
+    print 'Connected to remote host. You can start sending messages'
+    prompt()
+    
+    while 1:
+        socket_list = [sys.stdin, sock]
+        # Get the list sockets which are readable
+        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+         
+        for s in read_sockets:             
+            if s == sock:
+                # incoming message from remote server, s
+                data = s.recv(recv_buffer)
+                if not data :
+                    print '\nDisconnected from chat server'
+                    sys.exit()
+                else :
+                    #print data
+                    sys.stdout.write('\r' + data)
+                    prompt()
+            
+            else :
+                # user entered a message
+                msg = sys.stdin.readline()
+                sock.send(msg)
+                prompt()
+                
--- Assignment 1 - Socket Programming/server.py
+++ Assignment 1 - Socket Programming/server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import socket, threading, sys
+import socket, threading, sys, select
 
 # Use argument to define port
 args = sys.argv
@@ -12,29 +12,65 @@ else:
     host = '0.0.0.0'
     port = int(args[1])
 
-# Create thread class to handle each connected client
-class ClientThread(threading.Thread):
-    def __init__(self, ip, port, socket):
-        threading.Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.socket = socket
-        print "[+] %s:%s - New thread is started" % (ip, str(port))
+MODE_CHAT = 1
+MODE_FILE = 2
 
-    def run(self):
-        # Define send and recv handler for each thread
-        self.socket.send('Welcome, Thank you for connecting...')
-        msg = self.socket.recv(1024)
-        print "[+] %s:%s - Received message : %s" % (ip, str(port), msg)
+sockets = []
+recv_buffer = 4096
 
-# Create socket server
-sock_server = socket.socket()
-sock_server.bind((host, port))
-sock_server.listen(5)
-print 'Socket server is listening at %s:%s' % (host, port)
+# broadcast chat messages to all connected clients
+def broadcast (sock_server, sock, message):
+    for socket in sockets:
+        # send the message only to peer
+        if socket != sock_server and socket != sock :
+            try :
+                socket.send(message)
+            except :
+                # broken socket connection
+                socket.close()
+                # broken socket, remove it
+                if socket in sockets:
+                    sockets.remove(socket)
 
-# Wait for client
-while True:
-    (sock, (ip, port)) = sock_server.accept()
-    thread = ClientThread(ip, port, sock)
-    thread.start()
+if __name__ == "__main__":
+    # Create socket server
+    sock_server = socket.socket()
+    sock_server.bind((host, port))
+    sock_server.listen(5)
+    print 'Socket server is listening at %s:%s' % (host, port)
+
+    sockets.append(sock_server)
+
+    while True:
+        read_sockets, write_sockets, error_sockets = select.select(sockets, [], [])
+        for sock in read_sockets:
+            # a new connection request recieved
+            if sock == sock_server: 
+                new_sock, addr = sock_server.accept()
+                sockets.append(new_sock)
+                print "[+] Client %s:%s connected" % addr
+                 
+                broadcast(sock_server, new_sock, "[%s:%s]\t: Entered our chatting room\n" % addr)
+                
+            # a message from a client, not a new connection
+            else:
+                cip, cport = sock.getpeername()
+                # process data recieved from client, 
+                try:
+                    # receiving data from the socket.
+                    data = sock.recv(recv_buffer)
+                    if data:
+                        # there is something in the socket
+                        broadcast(sock_server, sock, "[%s:%s]\t: %s" % (cip, cport, data)) 
+                    else:
+                        # remove the socket that's broken    
+                        if sock in sockets:
+                            sockets.remove(sock)
+                        # at this stage, no data means probably the connection has been broken
+                        broadcast(sock_server, sock, "[%s:%s]\t: Now offline\n" % (cip, cport)) 
+
+                # exception 
+                except:
+                    broadcast(sock_server, sock, "[%s:%s]\t: Now offline\n" % (cip, cport))
+                    continue
+                    
